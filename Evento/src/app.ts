f8ac3415cc524072f707248fdb42307c7edc83b7
Sebastian Guzdek <sebag93@gmail.com>
2020-09-21 20:00:08 +0200

    2.2 Implementacja routingu

--- Evento/src/app.ts
+++ Evento/src/app.ts
@@ -1,3 +1,11 @@
+import {RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  public message = 'Hello World!';
+  configureRouter(config: RouterConfiguration){
+    config.title = 'Evento';
+    config.map([
+      {route: ['', 'home'], moduleId: './home'},
+      {route: 'users/register', moduleId: './users/view-models/register'}
+    ]);
+  }
 }
