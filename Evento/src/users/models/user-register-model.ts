90aaf31959fbd3dad0d0005661795f72db2b647e
Sebastian Guzdek <sebag93@gmail.com>
2020-09-21 20:48:45 +0200

    2.5 Implementacja widoku rejestracji użytkwonika

---
+++ Evento/src/users/models/user-register-model.ts
@@ -0,0 +1,6 @@
+export class UserRegisterModel {
+    login: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
--- Evento/src/users/view-models/register.ts
+++ Evento/src/users/view-models/register.ts
@@ -1,1 +1,13 @@
-export class UsersRegisterViewModel {}
+import { UserRegisterModel } from "../models/user-register-model";
+
+export class UsersRegisterViewModel {
+    private model: UserRegisterModel;
+
+    constructor() {
+        this.model = new UserRegisterModel();
+    }
+
+    register(){
+        alert(this.model.login);
+    }
+}
