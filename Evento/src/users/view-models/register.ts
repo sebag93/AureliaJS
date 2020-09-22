424857d04c9344771b13cbcd2b3e1a3fc33c97ce
Sebastian Guzdek <sebag93@gmail.com>
2020-09-22 23:38:25 +0200

    4.3 Definicja reguł walidacyjnych dla widoku...

--- Evento/src/users/view-models/register.ts
+++ Evento/src/users/view-models/register.ts
@@ -1,14 +1,21 @@
 import {autoinject} from 'aurelia-dependency-injection';
 import { UserRegisterModel } from '../models/user-register-model';
 import { HttpClient } from 'aurelia-fetch-client';
-import { IUserService, UsersService} from 'users/services/users-service';
+import { IUserService, UsersService} from '../services/users-service';
+import { RegisterViewValidator} from '../validators/register-view-validator';
+import { ValidationControllerFactory, ValidationController} from 'aurelia-validation';
+
 
 @autoinject()
 export class UsersRegisterViewModel {
+    private validationController: ValidationController;
     private model: UserRegisterModel;
 
-    constructor(private usersService: UsersService) {
+    constructor(private usersService: UsersService, validator: RegisterViewValidator, 
+        validationControllerFactory: ValidationControllerFactory) {
         this.model = new UserRegisterModel();
+        this.validationController = validationControllerFactory.createForCurrentScope();
+        validator.validate(this.model);
     }
 
     async register(){
