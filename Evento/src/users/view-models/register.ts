ecc0aa0130adc5a4e41f34f5f288ca730d064355
Sebastian Guzdek <sebag93@gmail.com>
2020-09-22 20:19:33 +0200

    3. Komunikacja z API 3.1 Instalacja aurelia-fetch-client

--- Evento/src/users/view-models/register.ts
+++ Evento/src/users/view-models/register.ts
@@ -1,10 +1,15 @@
-import { UserRegisterModel } from "../models/user-register-model";
+import { UserRegisterModel } from '../models/user-register-model';
+import { HttpClient } from 'aurelia-fetch-client'
 
 export class UsersRegisterViewModel {
     private model: UserRegisterModel;
 
     constructor() {
         this.model = new UserRegisterModel();
+
+        new HttpClient().fetch("https://api.github.com/users/sebag93/repos")
+            .then<any>(response => response.json())
+            .then(r => console.log(r));
     }
 
     register(){
