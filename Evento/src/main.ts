f3a67c14f6405fc3c749642813291f71d25ed549
Sebastian Guzdek <sebag93@gmail.com>
2020-09-22 23:01:32 +0200

    4. Walidacja danych 4.2 Instalacja aurelia-validation

--- Evento/src/main.ts
+++ Evento/src/main.ts
@@ -1,13 +1,15 @@
 import {Aurelia} from 'aurelia-framework';
 import environment from './environment';
 
-export function configure(aurelia: Aurelia): void {
+export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature('resources');
+    .plugin('aurelia-validation');
 
-  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
-
+  if (environment.debug){
+    aurelia.use.developmentLogging();
+  }
+  
   if (environment.testing) {
     aurelia.use.plugin('aurelia-testing');
   }
