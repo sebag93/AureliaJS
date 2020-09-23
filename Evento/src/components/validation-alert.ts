0a7f01c232710991d22d688dfe8e581f57170e5b
Sebastian Guzdek <sebag93@gmail.com>
2020-09-23 08:41:40 +0200

    5.2 Tworzenie uniwersalnego komponentu walidacyjne

---
+++ Evento/src/components/validation-alert.ts
@@ -0,0 +1,9 @@
+import { ValidationController } from "aurelia-validation";
+
+export class ValidationAlertViewModel{
+    private validationController: ValidationController;
+
+    activate(validationController: ValidationController) {
+        this.validationController = validationController;
+    }
+}
